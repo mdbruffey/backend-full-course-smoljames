1e6c954994bce5c3a2f1bad4cad1ae26c5faf5e1
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2025-03-27 15:33:11 -0500

    Update to use Prisma ORM and PSQL and Dockerized

--- chapter_4/src/routes/authRoutes.ts
+++ chapter_4/src/routes/authRoutes.ts
@@ -1,24 +1,21 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import db from "../db";
+import db from "../db.ts";
+import { hash } from "crypto";
+import prisma from "../prismaClient.ts";
 
 const router = express.Router();
 
-interface User {
-    id: number;
-    username: string;
-    password: string;
-}
-
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
         if (!username || !password)
             throw new Error("Missing username or password");
 
-        const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
-        const user = <User>getUser.get(username);
+        const user = await prisma.user.findUnique({
+            where: { username: username },
+        });
 
         //If no associated user, exit the function and return a 404
         if (!user) {
@@ -38,32 +35,37 @@ router.post("/login", (req, res) => {
         const token = jwt.sign({ id: user.id }, key, {
             expiresIn: "24h",
         });
-        res.status(200).json({token: token});
+        res.status(200).json({ token: token });
     } catch (error) {
         res.status(503).json({ error: error });
     }
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         const { username, password } = req.body;
         if (!username || !password)
             throw new Error("Missing information required to create user");
         const hashedPassword = bcrypt.hashSync(password, 8);
-        const insertUser = db.prepare(
-            `INSERT INTO users (username, password) VALUES(?,?)`
-        );
-        const result = insertUser.run(username, hashedPassword);
+
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: hashedPassword,
+            },
+        });
 
         const defaultTodo = `Hello :) Add your first todo!`;
-        const insertTodo = db.prepare(
-            `INSERT INTO todos (user_id, task) VALUES (?,?)`
-        );
-        insertTodo.run(result.lastInsertRowid, defaultTodo);
+        await prisma.todo.create({
+            data: {
+                task: defaultTodo,
+                userId: user.id,
+            },
+        });
 
         //create JWT token
         const key = process.env.JWT_SECRET || "default";
-        const token = jwt.sign({ id: result.lastInsertRowid }, key, {
+        const token = jwt.sign({ id: user.id }, key, {
             expiresIn: "24h",
         });
         res.status(201).json({ token: token });
--- chapter_4/src/routes/todoRoutes.ts
+++ chapter_4/src/routes/todoRoutes.ts
@@ -1,43 +1,48 @@
 import express from "express";
-import db from "../db";
-import { get } from "http";
+import prisma from "../prismaClient.ts";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+//Get all Todos for a user
+router.get("/", async (req, res) => {
     try {
-        const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`);
-        const todos = getTodos.all(req.body.userId);
+        const todos = await prisma.todo.findMany({
+            where: { userId: req.body.userId },
+        });
         res.json(todos);
     } catch (error) {
         res.status(503).json({ error: error });
     }
 });
 
-router.post("/", (req, res) => {
+//Create Todo
+router.post("/", async (req, res) => {
     //Should I consider checking that the user ID is legitimate? In this situation it's common
     //that a token I handed out persists after the server restarts and the user ceases to exist, lol.
     //I suppose it will error out anyway because the user ID is a foreign relation and won't exist
     //in the referenced table...
     try {
-        const insertTodo = db.prepare(
-            `INSERT INTO todos (user_id, task) VALUES(?,?)`
-        );
-        const result = insertTodo.run(req.body.userId, req.body.task);
+        await prisma.todo.create({
+            data: {
+                task: req.body.task,
+                userId: req.body.userId,
+            },
+        });
         res.status(201).json({ message: "Task created" });
     } catch (error) {
         res.status(503).json({ error: error });
     }
 });
 
-router.put("/:task_id", (req, res) => {
+//Update Todo
+router.put("/:task_id", async (req, res) => {
     try {
         const { completed, userId } = req.body;
         const { task_id } = req.params;
-        const updateTodo = db.prepare(
-            `UPDATE todos SET completed = ? WHERE id = ? AND user_id = ?`
-        );
-        const result = updateTodo.run(completed, task_id, userId);
+        await prisma.todo.update({
+            where: { id: parseInt(task_id) },
+            data: { completed: !!completed },
+        });
         res.status(200).json({
             message: `Task with id ${task_id} marked as ${
                 completed ? "complete" : "incomplete"
@@ -48,13 +53,18 @@ router.put("/:task_id", (req, res) => {
     }
 });
 
-router.delete("/:task_id", (req, res) => {
+//Delete a Todo
+router.delete("/:task_id", async (req, res) => {
     try {
         const { userId } = req.body;
         const { task_id } = req.params;
-        const delTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`);
-        const result = delTodo.run(task_id, userId);
-        res.status(200).json({message: `Task with id ${task_id} deleted`})
+        await prisma.todo.delete({
+            where: {
+                id: parseInt(task_id),
+                userId: parseInt(userId),
+            },
+        });
+        res.status(200).json({ message: `Task with id ${task_id} deleted` });
     } catch (error) {
         res.status(503).json({ error: error });
     }
